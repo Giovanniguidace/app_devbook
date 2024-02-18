d24baf0eb3cf1367f8042e60f58c13d9d78e272c
giovanni <giovannimarassi@gmail.com>
2024-02-17 22:53:26 -0300

    criando cookies utilizando hashkey - autenticacao

--- webapp/src/controllers/login.go
+++ webapp/src/controllers/login.go
@@ -4,8 +4,10 @@ import (
 	"bytes"
 	"encoding/json"
 	"fmt"
-	"io/ioutil"
 	"net/http"
+	"webapp/src/config"
+	"webapp/src/cookies"
+	"webapp/src/modelos"
 	"webapp/src/respostas"
 )
 
@@ -13,7 +15,7 @@ import (
 func FazerLogin(w http.ResponseWriter, r *http.Request) {
 	r.ParseForm()
 
-	usuario, erro := json.Marshal(map[string]string {
+	usuario, erro := json.Marshal(map[string]string{
 		"email": r.FormValue("email"),
 		"senha": r.FormValue("senha"),
 	})
@@ -22,16 +24,30 @@ func FazerLogin(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	response, erro := http.Post("http://localhost:5000/login", "application/json", bytes.NewBuffer(usuario))
-	if erro != nil{
+	url := fmt.Sprintf("%s/login", config.APIURL)
+	response, erro := http.Post(url, "application/json", bytes.NewBuffer(usuario))
+	if erro != nil {
 		respostas.JSON(w, http.StatusInternalServerError, respostas.ErroAPI{Erro: erro.Error()})
 	}
 
 	defer response.Body.Close()
 
-	token, _ := ioutil.ReadAll(response.Body)
+	if response.StatusCode >= 400 {
+		respostas.TratarStatusCodeDeErro(w, response)
+		return
+	}
 
-	fmt.Println(response.StatusCode, string(token))
+	var dadosAutenticacao modelos.DadosAutenticacao
+	if erro = json.NewDecoder(response.Body).Decode(&dadosAutenticacao); erro != nil {
+		respostas.JSON(w, http.StatusUnprocessableEntity, respostas.ErroAPI{Erro: erro.Error()})
+		return
+	}
 
+	if erro = cookies.Salvar(w, dadosAutenticacao.ID, dadosAutenticacao.Token); erro != nil {
+		respostas.JSON(w, http.StatusUnprocessableEntity, respostas.ErroAPI{Erro: erro.Error()})
+		return 
+	}
 	
+	respostas.JSON(w, http.StatusOK, nil)
+
 }
